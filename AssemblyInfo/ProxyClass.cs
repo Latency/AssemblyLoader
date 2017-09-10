f5794c2d2898328bc32c23a793cdd50a7009eb39
Latency McLaughlin <latency@bio-hazard.us>
2017-09-10 16:43:12 -0700

    Update test driver and API.

---
+++ AssemblyInfo/ProxyClass.cs
@@ -0,0 +1,111 @@
+﻿//  *****************************************************************************
+//  File:       Proxy.cs
+//  Solution:   AssemblyInfo
+//  Project:    AssemblyInfo
+//  Date:       09/07/2017
+//  Author:     Latency McLaughlin
+//  Copywrite:  Bio-Hazard Industries - 1998-2017
+//  *****************************************************************************
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace AssemblyInfo {
+  [Serializable, ClassInterface(ClassInterfaceType.AutoDual)]
+  internal class ProxyClass : MarshalByRefObject, IDisposable {
+    public enum InstanceStatus {
+      Loaded,
+      Unloaded,
+      Error
+    }
+
+
+    // some fields that require cleanup
+    private bool _disposed; // to detect redundant calls
+
+
+    #region .ctor
+    // ---------------------------------------------------------------
+
+    public ProxyClass() {
+    }
+
+
+    public ProxyClass(string assemblyFile, string domainName, string configFile = null) {
+      Status = InstanceStatus.Unloaded;
+      try {
+        AssemblyFile = assemblyFile; //full path to assembly
+        AssemblyFileName = System.IO.Path.GetFileName(AssemblyFile); //assmbly file name
+        Path = System.IO.Path.GetDirectoryName(AssemblyFile); //get root directory from assembly path
+
+        //start to configure domain
+        var appDomainInfo = new AppDomainSetup {
+          ApplicationBase = Path,
+          PrivateBinPath = Path,
+          PrivateBinPathProbe = Path,
+          ShadowCopyFiles = "true",
+          LoaderOptimization = LoaderOptimization.MultiDomainHost
+        };
+        if (!string.IsNullOrEmpty(configFile)) {
+          appDomainInfo.ConfigurationFile = configFile;
+        }
+        //lets create the domain
+        AtomicAppDomain = AppDomain.CreateDomain(domainName, null, appDomainInfo);
+
+        // Instanciate the class.
+        InstancedObject = (ProxyClass)AtomicAppDomain.CreateInstanceFromAndUnwrap(AssemblyFile, typeof(ProxyClass).ToString());
+        Status = InstanceStatus.Loaded;
+      } catch (Exception exception) {
+        //There was a problema setting up the new appDomain
+        Status = InstanceStatus.Error;
+        LoadingErrors = exception.ToString();
+      }
+    }
+    #endregion
+
+
+    #region Properties
+    // ---------------------------------------------------------------
+
+    public string AssemblyFile { get; }
+
+    public string AssemblyFileName { get; }
+
+    public AppDomain AtomicAppDomain { get; }
+
+    public dynamic InstancedObject { get; }
+
+    public string LoadingErrors { get; }
+
+    public InstanceStatus Status { get; }
+
+    public string Path { get; }
+
+    // ---------------------------------------------------------------
+    #endregion Properties
+
+
+    #region Methods
+    // ---------------------------------------------------------------
+
+    protected virtual void Dispose(bool disposing) {
+      if (!_disposed) {
+        if (disposing) {
+          // Dispose managed resources.
+          AppDomain.Unload(AtomicAppDomain);
+        }
+        
+        _disposed = true;
+      }
+    }
+
+
+    public void Dispose() {
+      Dispose(true);
+      GC.SuppressFinalize(this);
+    }
+
+    // ---------------------------------------------------------------
+    #endregion Methods
+  }
+}
--- README.md
+++ README.md
@@ -5,24 +5,56 @@
 ---
 
 * CREATED BY: Latency McLaughlin
-* FRAMEWORK:  .NET 4.6.1
-* SUPPORTS:   Visual Studio 2015, 2013, 2012, 2010, 2008
-* UPDATED:    3/1/2016
+* FRAMEWORK:  .NET 4.7
+* SUPPORTS:   Visual Studio 2017, 2015, 2013, 2012, 2010, 2008
+* UPDATED:    9/7/2017
 * TAGS:       C# .NET AssemblyInfo API
 * VERSION:    1.0.6457.35901
 
 <hr>
 
 ## Navigation
+* <a href="#history">History</a>
 * <a href="#introduction">Introduction</a>
+* <a href="#solution">Solution</a>
 * <a href="#installation">Installation</a>
 * <a href="#license">License</a>
 
 <hr>
 
+<h2><a name=history">History</a></h2>
+
+Trying to get information out of an assembly without loading it into the current application domain is not that simple.
+There is no way to get custom assembly attributes without loading it into current AppDomain.
+There is a special assembly loading method, <i>Assembly.ReflectionOnlyLoad()</i>, which uses a "reflection-only" load context.
+This lets you load assemblies that cannot be executed, but can have their metadata read.
+You cannot get typed attributes from this kind of assembly, only CustomAttributeData.
+That class doesn't provide any good way to filter for a specific attribute.
+
+Even worse, a reflection-only load does not load any dependency assemblies, but it forces you to do it manually.
+That makes it objectively worse than <i>Assembly.Load()</i> which at least gets the dependencies to load automatically.
+
+MEF includes a whole ton of custom reflection code to make this work.
+
+Ultimately, you cannot unload an assembly once it has been loaded.
+You need to unload the entire app domain, as described in this [MSDN article].
+
 <h2><a name="introduction">Introduction</a></h2>
 
 Gets assembly manifest information at runtime commonly found in *Properties* within .NET assemblies.
+
+<h2><a name="solution">Solution</a></h2>
+
+Based on the following premises:
+
+Creating an assembly proxy (or wrapper), derived from MarshalByRefObject, so that the CLR can marshal it by reference across AppDomain boundaries
+Loading the assembly within this proxy (Assembly.ReflectionOnlyLoadFrom)
+Performing the reflection inside this proxy and return the data you need
+Creating a temporary AppDomain and instantiatign the assembly proxy in this AppDomain (AppDomain.CreateInstanceFrom)
+Unloading the AppDomain as soon as you finished reflecting.
+However, you have to keep in mind that reflection on the assembly loaded this way is only possible inside the proxy (the one derived from MarshalByRefObject).
+It is not possible to return any "reflection object" (anything defined in the System.Reflection namespace, such as Type, MethodInfo, etc.).
+Trying to access these from another AppDomain (the caller's domain) would result in exceptions.
 
 <h2><a name="installation">Installation</a></h2>
 
@@ -51,3 +83,4 @@ Select the reference from the combo-box *Package Source* to the new entry.
 [//]: # (These are reference links used in the body of this note and get stripped out when the markdown processor does its job.)
 
    [GNU LESSER GENERAL PUBLIC LICENSE]: <http://www.gnu.org/licenses/lgpl-3.0.en.html>
+   [MSDN article]: <https://msdn.microsoft.com/en-us/library/c5b8a8f9(v=vs.100).aspx>
