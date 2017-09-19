6988508e0443309d0120729b7886e1dde332de16
Latency McLaughlin <latency@bio-hazard.us>
2017-09-18 22:08:47 -0700

    Updates NuGet to support .NET Core 2.0 and .NET Standard 2.0

---
+++ AssemblyLoader-Standard/Load.cs
@@ -0,0 +1,89 @@
+﻿//  *****************************************************************************
+//  File:       Load.cs
+//  Solution:   AssemblyLoader
+//  Project:    AssemblyLoader
+//  Date:       09/10/2017
+//  Author:     Latency McLaughlin
+//  Copywrite:  Bio-Hazard Industries - 1998-2017
+//  *****************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace AssemblyLoader {
+  public static class Load {
+    /// <summary>
+    ///   Load`
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="assembly"></param>
+    /// <returns></returns>
+    public static Assembly Assembly<T>(T assembly) {
+      var assemblypath = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+      // Each assemblypath (as it says..) is the full path to the assembly
+      using (var isoDomain = new ProxyClass(assemblypath)) {
+        Assembly myAssembly = null;
+
+        var type = typeof(T);
+        if (type == typeof(string)) {
+          var filePath = assembly as string;
+          if (!string.IsNullOrEmpty(filePath)) {
+            try {
+              var data = File.ReadAllBytes(filePath);
+              isoDomain.Dispose();
+              myAssembly = Assembly(data);
+            } catch (Exception) {
+              // ignored
+            }
+          }
+        } else if (type == typeof(byte[])) {
+          var data = assembly as byte[];
+          if (data != null && data.Length > 0) {
+            try {
+              myAssembly = System.Reflection.Assembly.Load(data);
+            } catch (Exception) {
+              // ignored
+            }
+          }
+        } else if (type == typeof(AssemblyName)) {
+          // This should always be null, since there are no embedded assemblies within AssemblyInfo or contains the name as that found in Tests. 
+          // Since we are running these with VST in a proxy rather than assemblies loaded in the executing assembly, loaded assemblies can not be unloaded
+          // without bringing down the domain.   There must be a host domain to call our methods.
+          //
+          // Leaving this one in for backwords compatability, which will mean that any local referenced assembly will need to be converted from <string>
+          // to <AssemblyName> in order to be used.
+          //
+          // This should always fail as no dependancies exist.
+          //
+          // Therefore, I will overload the Assenbly.Load(<string>) to be an explicit file path rather than namespace display name assembly reference.
+          var assemblyName = assembly as AssemblyName;
+          if (assemblyName != null) {
+            try {
+              myAssembly = System.Reflection.Assembly.Load(assemblyName);
+            } catch (Exception) {
+              // ignored
+            }
+          }
+        } else if (type == typeof(Stream)) {
+          var asmStream = assembly as Stream;
+          if (asmStream != null) {
+            var data = new byte[asmStream.Length];
+            asmStream.Read(data, 0, data.Length);
+            isoDomain.Dispose();
+            myAssembly = Assembly(data);
+          }
+        }
+        return myAssembly;
+      }
+    }
+
+
+    public static IEnumerable<Assembly> Assembly<T>(IEnumerable<T> obj) {
+      // ReSharper disable once LoopCanBeConvertedToQuery
+      foreach (var o in obj)
+        yield return Assembly(o);
+    }
+  }
+}
---
+++ Tests-Core/Program.cs
@@ -0,0 +1,61 @@
+﻿//  *****************************************************************************
+//  File:       Program.cs
+//  Solution:   AssemblyLoader
+//  Project:    Tests
+//  Date:       09/10/2017
+//  Author:     Latency McLaughlin
+//  Copywrite:  Bio-Hazard Industries - 1998-2017
+//  *****************************************************************************
+
+using System;
+using System.IO;
+using System.Reflection;
+using AssemblyLoader;
+using NUnit.Framework;
+
+namespace Tests {
+  public class Program {
+    [Test]
+    public void Test1() {
+      // Dynamically find the embedded resource assembly included within the project to test against.
+      var assembly = Assembly.GetExecutingAssembly();
+      foreach (var s in assembly.GetManifestResourceNames())
+        using (var stream = assembly.GetManifestResourceStream(s)) {
+          if (stream != null) {
+            // Test overload +1 - Stream
+            var asm = stream.Assembly();
+            Console.WriteLine(asm.FullName);
+
+            asm = asm.GetName().Assembly();
+            Console.WriteLine(asm.FullName);
+
+            var data = new byte[stream.Length];
+            stream.Seek(0, SeekOrigin.Begin);
+            stream.Read(data, 0, data.Length);
+            var fileName = Path.GetTempPath() + s.Substring(s.IndexOf('.') + 1);
+            if (File.Exists(fileName)) // Overwrite if desired  (depending on your needs)
+              File.Delete(fileName);
+            File.WriteAllBytes(fileName, data);
+            // Test overload +4 - string
+            asm = fileName.Assembly();
+            Console.WriteLine(asm.FullName);
+          }
+        }
+    }
+
+
+    [Test]
+    public void Extensions() {
+      var asm = Assembly.GetAssembly(typeof(Extensions));
+
+      Console.WriteLine($"ProductTitle:  {asm.ProductTitle()}");
+      Console.WriteLine($"AssemblyVersion:  {asm.AssemblyVersion()}");
+      Console.WriteLine($"AssemblyFileVersion:  {asm.AssemblyFileVersion()}");
+      Console.WriteLine($"Description:  {asm.Description()}");
+      Console.WriteLine($"Product:  {asm.Product()}");
+      Console.WriteLine($"Copyright:  {asm.Copyright()}");
+      Console.WriteLine($"Company:  {asm.Company()}");
+      Console.WriteLine($"Trademark:  {asm.Trademark()}");
+    }
+  }
+}
